011a6cb04a7678e8a144b73e2cbf6fa50b7f6ca4
caro-turnbull <caro.turnbull@gmail.com>
2023-09-19 12:09:34 -0400

    ex5 refactored with scope

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,14 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+puts "total revenue: #{Store.sum("annual_revenue")}"
+puts "average revenue: #{Store.average("annual_revenue")}"
+
+class Store
+scope :over1M , -> { where('annual_revenue > ?', 1000000) } 
+#can call this scope "over1M" later
+end
+
+for store in Store.over1M do
+  puts "over 1M: #{store.name}"
+end
