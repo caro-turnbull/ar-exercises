18611452fc5a36d1f9319f6a6633dcf064c4f4dc
caro-turnbull <caro.turnbull@gmail.com>
2023-09-19 10:59:49 -0400

    ex2 done

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -7,12 +7,9 @@ puts "----------"
 # end
   
   Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
-  Store.create(name: "Burnaby", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+  Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
   Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
-  
-  # s.location = :location
-  # s.annual_revenue = annual_revenue
-  # s.items = items
+
   
   puts "COUNT -> #{Store.count}"
   
