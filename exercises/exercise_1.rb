afce64ae7ba4951d1f7afbc15281cc402975c778
caro-turnbull <caro.turnbull@gmail.com>
2023-09-19 10:53:21 -0400

    ex1 done

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,16 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
+# class Store < ApplicationRecord
+# end
+  
+  Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+  Store.create(name: "Burnaby", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+  Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+  
+  # s.location = :location
+  # s.annual_revenue = annual_revenue
+  # s.items = items
+  
+  puts "COUNT -> #{Store.count}"
+  
