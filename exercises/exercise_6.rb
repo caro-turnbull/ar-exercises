1c3eb058eb00790be1871ce3dc1f1ed36c9507a9
caro-turnbull <caro.turnbull@gmail.com>
2023-09-19 12:15:55 -0400

    ex6 done

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,20 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+class Store
+has_many :employees
+end
+
+class Employee 
+belongs_to :store
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Caro ", last_name: "Tbull", hourly_rate: 100)
+@store1.employees.create(first_name: "sue", last_name: "blue", hourly_rate: 40)
+@store1.employees.create(first_name: "jan", last_name: "fran", hourly_rate: 60)
+@store2.employees.create(first_name: "Marshall", last_name: "Tbull", hourly_rate: 10)
+@store2.employees.create(first_name: "Chris", last_name: "Topher", hourly_rate: 20)
+@store2.employees.create(first_name: "Bob", last_name: "Fred", hourly_rate: 50)
+
+# puts Employee.count
